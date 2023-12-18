b493640df121084bd0356bc986c80f54c890daca
AntoineDehan <antoine.dehandartial@gmail.com>
2023-12-18 15:41:05 +0100

    fonctions

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
 const listeMots = ["Cachalot","Pétunia","Serviette"]
 const ListePhrases = ["Pas de panique !","La vie, l'univers et le reste","Merci pour le poisson"]
-let score = 0
 
-let choix = prompt("Veuillez choisir la liste : mots ou phrases")
-while (choix !== "choix" && choix !== "phrases") {
-    choix = prompt("Veuillez choisir la liste : mots ou phrases")
+
+function afficherResultat(score, nbMotsProposes) {
+    console.log("Votre score est de : " + score + " sur " + nbMotsProposes)
 }
 
-if (choix === "mots") {
-    for (let i = 0; i < listeMots.length; i++) {
-        let motUtilisateur = prompt("Ecrivez le mot : " + listeMots[i])
-        if (motUtilisateur === listeMots[i]) {
+function choisirPhrasesOuMots() {
+    let choix = prompt("Veuillez choisir la liste : mots ou phrases")
+    while (choix !== "mots" && choix !== "phrases") {
+        choix = prompt("Veuillez choisir la liste : mots ou phrases")
+    }   
+        return choix
+}
+
+function LancerBoucleJeu(listePropositions) {
+    let score = 0
+    for (let i = 0; i < listePropositions.length; i++) {
+        motUtilisateur = prompt("Ecrivez le mot : " + listePropositions[i])
+        if (motUtilisateur === listePropositions[i]) {
             score++
         }
     }
-} else  {
-    for (let i = 0; i < ListePhrases.length; i++) {
-        let motUtilisateur = prompt("Ecrivez la phrase : " + ListePhrases[i])
-        if (motUtilisateur === ListePhrases[i]) {
-            score++
-        }
+    return score
+}
+
+function lancerJeu() {
+    let choix = choisirPhrasesOuMots()
+    let score = 0
+    let nbMotsProposes = 0
+
+    if (choix === "mots") {
+        score = LancerBoucleJeu(listeMots)
+        nbMotsProposes = listeMots.length
+    } else {
+        score = LancerBoucleJeu(ListePhrases)
+        nbMotsProposes = ListePhrases.length
     }
+
+    afficherResultat(score, nbMotsProposes)
 }
 
 
-
-console.log("Votre score est de : " + score)
+lancerJeu();
