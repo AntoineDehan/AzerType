6fbd537ba5baa165b682e267a9f7ca5a0135ce6d
AntoineDehan <antoine.dehandartial@gmail.com>
2023-12-18 14:44:28 +0100

    Test phrases + mots (while/for)

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-const motApplication = "Bonjour"
-let motUtilisateur = prompt("Entrez le mot : " + motApplication)
+const listeMots = ["Cachalot","Pétunia","Serviette"]
+const ListePhrases = ["Pas de panique !","La vie, l'univers et le reste","Merci pour le poisson"]
+let score = 0
 
-if (motUtilisateur === motApplication) {
-    console.log("Bravo !")
-} else {
-    console.log("Vous avez fait une faute de frappe")
+let choix = prompt("Veuillez choisir la liste : mots ou phrases")
+while (choix !== "choix" && choix !== "phrases") {
+    choix = prompt("Veuillez choisir la liste : mots ou phrases")
 }
+
+if (choix === "mots") {
+    for (let i = 0; i < listeMots.length; i++) {
+        let motUtilisateur = prompt("Ecrivez le mot : " + listeMots[i])
+        if (motUtilisateur === listeMots[i]) {
+            score++
+        }
+    }
+} else  {
+    for (let i = 0; i < ListePhrases.length; i++) {
+        let motUtilisateur = prompt("Ecrivez la phrase : " + ListePhrases[i])
+        if (motUtilisateur === ListePhrases[i]) {
+            score++
+        }
+    }
+}
+
+
+
+console.log("Votre score est de : " + score)
